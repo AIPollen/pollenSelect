e85747a68c94724b2e9fedf6dc2a3ff7b7a77153
Marcory van Dijk <79036937+marcory-hub@users.noreply.github.com>
2024-03-24 11:33:01 +0100

    Prompt the user to select an image file

--- selectMultipleROIs.py
+++ selectMultipleROIs.py
@@ -1,48 +1,51 @@
 import cv2
 import numpy as np
 import os
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-# Image path
-img_path = "/Users/marcory/Documents/PollenOnderzoek/AIPollen/pollenSelect/pollenSelect/testImages/80euroAdapterSneeuwklokje.jpg"
+# Create a Tkinter window to access file dialogs
+root = Tk()
+root.withdraw()  # Hide the main window
 
-# Read image
-img_raw = cv2.imread(img_path)
+# Prompt the user to select an image file
+img_path = askopenfilename(title="Select an image")
 
-# Select ROIs function
-ROIs = cv2.selectROIs("Select Rois", img_raw)
+# Check if a file was selected
+if img_path:
+    img_raw = cv2.imread(img_path)
 
-# Ask for pollentype name
-pollentype_name = input("Enter the name of the pollentype: ")
+    # Select ROIs function
+    ROIs = cv2.selectROIs("Select Rois", img_raw)
 
-# Create a directory for the pollen type
-os.makedirs(pollentype_name, exist_ok=True)  # Create directory if it doesn't exist, otherwise do nothing
+    # Ask for pollentype name
+    pollentype_name = input("Enter the name of the pollentype: ")
 
-# Print rectangle points of selected ROIs
-print(ROIs)
+    # Create a directory for the pollen type
+    os.makedirs(pollentype_name, exist_ok=True)  # Create directory if it doesn't exist, otherwise do nothing
 
-# Crop selected ROIs from raw image
+    # Print rectangle points of selected ROIs
+    print(ROIs)
 
-# Counter to save images with different names
-crop_number = 0
+    # Crop selected ROIs from raw image
+    crop_number = 0
 
-# Loop over every bounding box saved in array "ROIs"
-for rect in ROIs:
-    x1 = rect[0]
-    y1 = rect[1]
-    x2 = rect[2]
-    y2 = rect[3]
+    for rect in ROIs:
+        x1, y1, x2, y2 = rect
 
-    # Crop ROI from original image
-    img_crop = img_raw[y1:y1+y2, x1:x1+x2]
+        # Crop ROI from original image
+        img_crop = img_raw[y1:y1+y2, x1:x1+x2]
 
-    # Show cropped image
-    cv2.imshow("crop"+str(crop_number), img_crop)
+        # Show cropped image
+        cv2.imshow("crop"+str(crop_number), img_crop)
 
-    # Save cropped image in the pollentype directory
-    cv2.imwrite(os.path.join(pollentype_name, f"{pollentype_name}_{crop_number}.jpeg"), img_crop)
+        # Save cropped image in the pollentype directory
+        cv2.imwrite(os.path.join(pollentype_name, f"{pollentype_name}_{crop_number}.jpeg"), img_crop)
 
-    crop_number += 1
+        crop_number += 1
 
-# Hold windows open
-cv2.waitKey(0)
+    cv2.waitKey(0)
+
+else:
+    print("No image selected.")
 
